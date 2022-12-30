f0b3b902f2a8521bf586e2c0157e9c35492bb21e
Progna_Labony_Roy <labony.progna@gmail.com>
2022-12-30 22:22:31 +0600

    code modified

--- index.js
+++ index.js
@@ -61,26 +61,47 @@ async function run() {
             console.log(result)
       })
 
-      app.put('/updateTask/:id', async (req, res) => {
-        const updateTask = req.body;
-        console.log(updateTask)
-        const filter = {
-            _id: ObjectId(updateTask.id)
-        }
-        const option = { upsert: true };
-        const updateDoc = {
-            $set: {
+    //   app.put('/updateTask/:id', async (req, res) => {
+    //     const updateTask = req.body;
+    //     console.log(updateTask)
+    //     const filter = {
+    //         _id: ObjectId(updateTask.id)
+    //     }
+    //     const option = { upsert: true };
+    //     const updateDoc = {
+    //         $set: {
 
-                title: updateTask.title,
-                email:updateTask.email,
-                description: updateTask.dscription
-            }
-        }
-        const result = await taskCollection.updateOne(filter, updateDoc, option);
-        res.send(result)
-        console.log(result);
-    })
+    //             title: updateTask.title,
+    //             email:updateTask.email,
+    //             description: updateTask.dscription
+    //         }
+    //     }
+    //     const result = await taskCollection.updateOne(filter, updateDoc, option);
+    //     res.send(result)
+    //     console.log(result);
+    // })
     
+
+    app.put('/updateTask', async (req, res) => {
+      const updateTask = req.body;
+      console.log(updateTask)
+      const filter = {
+          _id: ObjectId(updateTask.id)
+      }
+      const option = { upsert: false };
+      const updateDoc = {
+          $set: {
+            title: updateTask.title,
+              description: updateTask.description,
+          }
+      }
+      console.log(updateDoc)
+      const result = await taskCollection.updateOne(filter, updateDoc, option);
+      console.log(result);
+      res.send(result)
+      
+  })
+
 
   } finally {
   }
